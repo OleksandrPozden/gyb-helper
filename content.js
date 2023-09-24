dee1f2b5b3ec5f25947ea3c5f9bda32aafb9dd40
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-09-24 15:11:59 +0300

    release version 0.1.0

--- content.js
+++ content.js
@@ -3,12 +3,6 @@ let isWorking = false;
 let main = async () => {
   while (isWorking==true) {
     try {
-      console.log("started main process");
-
-      const result = await chrome.storage.local.get(["ActiveChats"])
-      const prevActiveChats = result.ActiveChats;
-      let curActiveChats = [];
-
       const rows = document.querySelectorAll('.css-14wsju2');
       for (let element of rows) {
         const id = element.getAttribute('data-testid');
@@ -20,32 +14,27 @@ let main = async () => {
           continue;
         }
         console.log(id)
-        curActiveChats.push(id);
-        if (!prevActiveChats.includes(id)) {
-          console.log("make an request")
-          const response = await fetch("http://127.0.0.1:5000", {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ "url": url })
-          });
+        const response = await fetch("http://127.0.0.1:5000", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ "url": url })
+        });
 
-          if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.status}`);
-          }
-
-          const data = await response.json();
-          if (data.message == true) {
-            const newText = document.createTextNode("LIFETIME");
-            const child = button.firstElementChild
-            button.insertBefore(newText, child)
-            // button.click()
-          }
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
         }
+
+        const data = await response.json();
+        if (data.message == true) {
+          const newText = document.createTextNode("LIFETIME");
+          const child = button.firstElementChild
+          button.insertBefore(newText, child)
+          // button.click()
+        }
+        
       }
-      console.log(curActiveChats)
-      await chrome.storage.local.set({ ActiveChats: curActiveChats })
     } catch (error) {
       console.error("An error occurred:", error);
     }
--- server.py
+++ server.py
@@ -1,12 +1,43 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
+from datetime import datetime
+from flask_caching import Cache
+
 
 app = Flask(__name__)
 CORS(app)
 
+cache = Cache(app, config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 31536000})  # 31536000 seconds = 1 year
+
+
+data = {
+    "total_requests": 0,
+    "data": []
+}
+def count(result, url):
+    data["total_requests"] += 1
+    data["data"].append([
+        datetime.now().strftime("%Y-%m-%d_%H-%M"),
+        result,
+        url
+    ])
+
+@app.route('/', methods=['GET'])
+def index():
+    result = f"total requests: {data['total_requests']}<br>"
+    result += "<br>".join([f"{entry[0]} | {entry[1]} | {entry[2]}" for entry in data["data"]])
+    return result
+
+@app.route('/clear', methods=['GET'])
+def clear():
+    data["data"] = []
+    data['total_requests'] = 0
+    return 'Cleared'
+
 @app.route('/', methods=["POST"])
-def hello_world():
+@cache.cached() 
+def check_url():
     data = request.get_json()
     url = data.get("url")
     if not url:
@@ -16,6 +47,7 @@ def hello_world():
         data = {"message": True}
     else:
         data = {"message": False}
+    count(data["message"], url)
     return jsonify(data)
 
 if __name__ == '__main__':
