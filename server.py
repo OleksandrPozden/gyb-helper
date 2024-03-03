b1f4d40afddadca7c857105f120773e8b18f0e14
oleksandrpozden <oleksandr.pozden@gmail.com>
2024-03-03 23:56:40 +0200

    version: 0.3.0 update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import requests
 from datetime import datetime
 import re
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -23,15 +24,33 @@ def count(result, url):
     ])
 
 def get_subscription(text):
+    pro_yearly = re.compile(r'<h5>.*?pro module.*?year.*?</h5>', re.DOTALL)
     if "lifetime" in text:
         return "LIFETIME"
+    if pro_yearly.search(text):
+        return "PRO_YEARLY"
     if "year" in text:
         return "YEARLY"
     if "month" in text:
         return "MONTHLY"
     return "NOTHING"
 
+def get_user_info(text):
+    result = {}
+    soup = BeautifulSoup(text, "lxml")
+    table = soup.find('table', class_="Webgood-Billing")
+    address_th_tag = table.find('th', string="Address:")
+    if address_th_tag:
+        address_td_tag = address_th_tag.find_next_sibling('td')
+        result["address"] = address_td_tag.get_text() if address_td_tag else ""
+    email_th_tag = table.find('th', string="Email:")
+    if email_th_tag:
+        email_td_tag = email_th_tag.find_next_sibling('td')
+        result["customer_email"] = email_td_tag.get_text() if email_td_tag else ""
+    return result
+
 def remove_link_and_script_tags(content):
+    content = content.lower()
     link_tag = re.compile(r'<link\b[^>]*>.*?</link>', re.DOTALL)
     script_tag = re.compile(r'<script\b[^>]*>.*?</script>', re.DOTALL)
     content = re.sub(script_tag, '', content)
@@ -59,9 +78,12 @@ def check_url():
     if url in cache:
         return jsonify(cache[url])
     r = requests.get(url)
-    text = r.text.lower()
-    message = get_subscription(remove_link_and_script_tags(text))
-    data = {"message": message}
+    user_info = get_user_info(r.text)
+    data = {
+        "message": get_subscription(remove_link_and_script_tags(r.text)),
+        "customer_email": user_info.get("customer_email", ""),
+        "address": user_info.get("address", "")
+    }
     cache[url] = data
     count(data["message"], url)
     return jsonify(data)
